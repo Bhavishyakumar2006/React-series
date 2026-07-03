6457e68a0f1061f7ddd1272e9b547bd13a541397
Bhavishyakumar2006 <bhavishyakumar32c@gmail.com>
2026-07-03 23:40:46 +0530

    add components

--- 07React_router/src/App.jsx
+++ 07React_router/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-
-import './App.css'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import { Outlet } from 'react-router'
+// import './App.css'
 
 function App() {
   
   return (
     <>
+    <Header />
+    <Outlet />
+    <Footer />
     </>
   )
 }
