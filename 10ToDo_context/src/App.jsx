9e639226752bca292a67b0bf626b6da12f61eaeb
Bhavishyakumar2006 <bhavishyakumar32c@gmail.com>
2026-08-02 23:55:52 +0530

    build ui with functionality

--- 10ToDo_context/src/App.jsx
+++ 10ToDo_context/src/App.jsx
@@ -6,7 +6,7 @@ import Todoform from "./components/Todoform";
 function App() {
   const [todos, setTodos] = useState([])
   const addTodo = (todo) => {
-    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
+    setTodos((prev) => [todo, ...prev])
   }
   const updateTodo = (id, todo) => {
     setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo))
--- 10ToDo_context/src/components/Todoform.jsx
+++ 10ToDo_context/src/components/Todoform.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
 
 function Todoform() {
+  const [todomsg, setTodomsg] = useState('')
+
+  const {addTodo} = useTodo()
+
+  const add = (e) => {
+    e.preventDefault()
+    if (todo && todo.length > 0) {
+      addTodo({id: Date.now(), todoMsg: todomsg, completed: false})
+      setTodomsg("")
+    }
+  }
   return (
-    <div>Todoform</div>
-  )
+    <form className="flex" onSubmit={add}>
+      <input
+        type="text"
+        placeholder="Write Todo..."
+        value={todomsg}
+        onChange={(e) => setTodomsg(e.target.value)}
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      >
+        Add
+      </button>
+    </form>
+  );
 }
 
-export default Todoform
+export default Todoform;
