2fee89f585a8db3b30f90c0f706ed51b12a80097
Bhavishyakumar2006 <bhavishyakumar32c@gmail.com>
2026-08-11 23:32:34 +0530

    learn .env and setup appwrite

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>The mega blog project</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+    appwrite_url: String(import.meta.env.VITE_APPWRITE_URL),
+    project_id: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    database_id: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    table_id: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+    bucket_id: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    
+}
+
+export default conf;
