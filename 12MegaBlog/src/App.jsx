a08226875ec9c287b94c18ad86c4b6d8c5586810
Bhavishyakumar2006 <bhavishyakumar32c@gmail.com>
2026-08-15 23:58:21 +0530

    work on store

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,37 @@
-
-import './App.css'
+import { login, logOut } from "../src/store/Authslice";
+import { useDispatch } from "react-redux";
+import { Header, Footer } from "./components/Index";
+import "./App.css";
+import {Outlet} from 'react-router'
+import { useEffect, useState } from "react";
+import authService from "../src/appwrite/Auth_service";
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <>
-      <h1>The mega blog project</h1>
-    </>
-  )
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          useDispatch(login(userData));
+        } else {
+          useDispatch(logOut());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div
+      className="min-h-screen flex flex-wrap content-between
+     bg-gray-400"
+    >
+     <Header />
+     <Outlet />
+     <Footer />  
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
